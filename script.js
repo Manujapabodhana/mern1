7f63289d4fb2ffe830d7dcf20d0512f807d56861
Manujapabodhana <rpmanuja123@gmail.com>
2025-08-02 08:48:01 +0530

    new

---
+++ script.js
@@ -0,0 +1,9 @@
+let omdpkey ="3bf7d695"
+
+function search() {
+    const input = document.getElementById('input');
+    const value = input.value;
+    
+    // Add your search logic here
+    console.log('Searching for:', value);
+}
