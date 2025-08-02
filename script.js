abb7d780e2bb3c925aa183ba240c62262703a355
Manujapabodhana <rpmanuja123@gmail.com>
2025-08-02 11:44:44 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,11 @@ let omdpkey ="3bf7d695"
 function search() {
     const input = document.getElementById('input');
     const value = input.value;
+
+    console.log(value);
+
+    let url = "https://www.omdbapi.com/?apikey=" + omdpkey + "&s=" + value;
+    console.log(url);
     
     // Add your search logic here
     console.log('Searching for:', value);
