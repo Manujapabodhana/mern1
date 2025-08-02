cad0ad2c67d11f3b2c83479e56ac4b1d69b35c05
Manujapabodhana <rpmanuja123@gmail.com>
2025-08-02 16:15:57 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,6 +8,18 @@ function search() {
 
     let url = "https://www.omdbapi.com/?apikey=" + omdpkey + "&s=" + value;
     console.log(url);
+
+    let httpRequest = new XMLHttpRequest()
+    httpRequest.open("GET", url)
+    httpRequest.responseType = "json"
+
+    httpRequest.onload = function() {
+        if (httpRequest.status === 200) {
+            console.log(httpRequest.response); 
+        }
+
+    
+
     
     // Add your search logic here
     console.log('Searching for:', value);
